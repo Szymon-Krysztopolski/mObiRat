31ccf4850ffc9b5ac821b753c2909186b5b82a21
Szymon-Krysztopolski <94224331+Szymon-Krysztopolski@users.noreply.github.com>
2023-10-21 11:30:14 +0200

    database init with example user entity

---
+++ backend/src/main/java/com/main/backend/features/database/DatabaseService.java
@@ -0,0 +1,14 @@
+package com.main.backend.features.database;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DatabaseService {
+    private final DatabaseRepository repository;
+
+    @Autowired
+    public DatabaseService(DatabaseRepository repository) {
+        this.repository = repository;
+    }
+}
