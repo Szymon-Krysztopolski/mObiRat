3d10960f636890919c1a9b66b52362dee43d706f
martai4 <tusiulka2011@gmail.com>
2023-10-21 17:19:19 +0200

    info page finished

--- backend/src/main/java/com/main/backend/features/database/domain/DatabaseController.java
+++ backend/src/main/java/com/main/backend/features/database/domain/DatabaseController.java
@@ -2,11 +2,13 @@ package com.main.backend.features.database.domain;
 
 import com.main.backend.features.database.pojo.BloodTypeData;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = {"http://localhost:3000"})
 @RequestMapping("/api")
 public class DatabaseController {
     DatabaseService service;
