de104283bd733b402ea5c8e1756ad5851108dc97
Szymon-Krysztopolski <94224331+Szymon-Krysztopolski@users.noreply.github.com>
2023-10-22 07:46:51 +0200

    logs

--- backend/src/main/java/com/main/backend/features/rckik/domain/RckikService.java
+++ backend/src/main/java/com/main/backend/features/rckik/domain/RckikService.java
@@ -1,11 +1,13 @@
 package com.main.backend.features.rckik.domain;
 
 import com.main.backend.features.rckik.entity.Rckik;
+import lombok.extern.log4j.Log4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Log4j
 @Service
 public class RckikService {
 
@@ -17,7 +19,10 @@ public class RckikService {
     }
 
     public Rckik getRckikByName(String name) {
-        return repository.findById(name).orElseGet(Rckik::getTestObject);
+        return repository.findById(name).orElseGet(() -> {
+            System.out.println("Name to check: "+name);
+            return Rckik.getTestObject();
+        });
     }
 
     public List<Rckik> getAllRckik() {
